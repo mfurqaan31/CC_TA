abbed939d5d50667229ab9ba5dd8c8581c2336dd
Mohammed Furqaan <furqaanpatel2004@gmail.com>
2025-03-13 14:44:11 +0530

    Create main.cpp

---
+++ main/main.cpp
@@ -0,0 +1,12 @@
+//File: hello.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Yello, World\n";
+  cout << "Yello, Jenkins\n";
+  cout << "Furqaan successfully built and run this  \n";
+  cout << "1 more commit  \n";
+  cout << "this is for task 5  \n";
+  return 0;
+}
