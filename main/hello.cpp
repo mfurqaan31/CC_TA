53e6c7db84528bc079b44fadb802f79d471d167a
mfurqaan31 <furqaanpatel2004@gmail.com>
2025-03-13 13:30:00 +0530

    feat: update

--- main/hello.cpp
+++ main/hello.cpp
@@ -3,8 +3,8 @@
 using namespace std;
 
 int main(void) {
-  cout << "Hello, World\n";
-  cout << "Hello, Jenkins\n";
-  cout << "I have successfully built and run this  \n";
+  cout << "Yello, World\n";
+  cout << "Yello, Jenkins\n";
+  cout << "Furqaan successfully built and run this  \n";
   return 0;
 }
